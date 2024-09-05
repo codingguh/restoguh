1ef6901008458180a4d28845ef47688416afcea0
codingguh <teguhharits91@gmail.com>
2024-09-05 21:11:00 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/restoguh/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.restoguh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'common/styles.dart';
+import 'data/model/restaurant.dart';
+import 'ui/restaurant_detail_page.dart';
+import 'ui/restaurant_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Restaurant App',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              primary: Colors.amber,
+              onPrimary: Colors.black,
+              secondary: secondaryColor,
+            ),
+        textTheme: myTextTheme,
+        appBarTheme: const AppBarTheme(elevation: 2),
+      ),
+      initialRoute: RestaurantListPage.routeName,
+      routes: {
+        RestaurantListPage.routeName: (context) => const RestaurantListPage(),
+        RestaurantDetailPage.routeName: (context) => RestaurantDetailPage(
+            restaurant:
+                ModalRoute.of(context)?.settings.arguments as Restaurant),
+      },
+    );
+  }
+}
